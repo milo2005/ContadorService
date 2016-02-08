d43144cae68bbc25099af52d6e8a948d344faca5
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-08 16:13:30 -0500

    Contador Servicio Implementado

--- app/src/main/java/com/example/estacionvl_tc_014/servicios/MainActivity.java
+++ app/src/main/java/com/example/estacionvl_tc_014/servicios/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.estacionvl_tc_014.servicios;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.estacionvl_tc_014.servicios.services.ContadorService;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView seconds;
+    ImageView stop;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seconds = (TextView) findViewById(R.id.seconds);
+        stop = (ImageView) findViewById(R.id.btn_stop);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setTag(false);
+
+        stop.setOnClickListener(this);
+        fab.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ContadorService.class);
+
+        if(v == stop){
+            intent.setAction(ContadorService.ACTION_STOP);
+        }else{
+            Boolean state = (Boolean) fab.getTag();
+            fab.setTag(!state);
+            if(state){
+                intent.setAction(ContadorService.ACTION_PAUSE);
+                fab.setImageResource(R.drawable.ic_play);
+            }else{
+                intent.setAction(ContadorService.ACTION_START);
+                fab.setImageResource(R.drawable.ic_pause);
+            }
+
+        }
+
+        startService(intent);
+
     }
 }
--- app/src/main/java/com/example/estacionvl_tc_014/servicios/services/ContadorService.java
+++ app/src/main/java/com/example/estacionvl_tc_014/servicios/services/ContadorService.java
@@ -2,7 +2,11 @@ package com.example.estacionvl_tc_014.servicios.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 
 public class ContadorService extends Service {
@@ -11,21 +15,42 @@ public class ContadorService extends Service {
     public static final String ACTION_PAUSE="pause";
     public static final String ACTION_STOP="stop";
 
-    public ContadorService() {
-    }
+    ContadorHandler contadorHandler;
 
+    boolean running;
+    boolean paused;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        HandlerThread thread = new HandlerThread("Contador"
+                , Thread.MAX_PRIORITY);
+        thread.start();
+        contadorHandler = new ContadorHandler(thread.getLooper());
+
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        if(intent.getAction().equals(ACTION_START)){
-            Log.i("ServicioContador","Accion Start");
+        if(intent.getAction().equals(ACTION_STOP)){
+            running = false;
+            stopSelf();
         }else if(intent.getAction().equals(ACTION_PAUSE)){
-            Log.i("ServicioContador","Accion Pause");
+            paused = true;
         }else{
-            Log.i("ServicioContador","Accion Stop");
-        }
 
+            if(paused && running)
+                paused = false;
+            else if(!running){
+                running = true;
+                Message msg =  contadorHandler.obtainMessage();
+                msg.obj = intent;
+                contadorHandler.sendMessage(msg);
+            }
+
+
+        }
         return START_REDELIVER_INTENT;
     }
 
@@ -33,4 +58,32 @@ public class ContadorService extends Service {
     public IBinder onBind(Intent intent) {
        return null;
     }
+
+    private class ContadorHandler extends Handler{
+
+        public ContadorHandler(Looper looper){
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            int seconds = 0;
+
+            while(running){
+                try {
+                    Thread.sleep(1000);
+                    if(!paused) {
+                        seconds++;
+                        Log.i("Segundos", "Segundos:" + seconds);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+    }
+
 }
