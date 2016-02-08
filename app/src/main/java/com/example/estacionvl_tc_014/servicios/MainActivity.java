20b1c1a283a69134acbc87469abdf6deb6974921
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-08 16:57:21 -0500

    Comunicacion Servicio - Activity implementada con Broadcast Receiver

--- app/src/main/java/com/example/estacionvl_tc_014/servicios/MainActivity.java
+++ app/src/main/java/com/example/estacionvl_tc_014/servicios/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.estacionvl_tc_014.servicios;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +9,16 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.estacionvl_tc_014.servicios.receivers.ContadorReceiver;
 import com.example.estacionvl_tc_014.servicios.services.ContadorService;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ContadorReceiver.ContadorI {
 
     TextView seconds;
     ImageView stop;
     FloatingActionButton fab;
+
+    ContadorReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         stop.setOnClickListener(this);
         fab.setOnClickListener(this);
 
+        receiver = new ContadorReceiver(this);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ContadorReceiver.ACTION_CONTADOR);
+
+        registerReceiver(receiver, filter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
     }
 
     @Override
@@ -53,5 +69,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         startService(intent);
 
+    }
+
+    @Override
+    public void onReceiveSeconds(int s) {
+        seconds.setText(""+s);
     }
 }
